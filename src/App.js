e40d1945613b5aa2da9676a338ed62e8cf36a4bc
rzmilon <rzmilon@gmail.com>
2023-02-03 23:04:51 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,39 +1,25 @@
-import React from "react";
-import { Element } from "react-scroll";
 import logo from './logo.svg';
 import './App.css';
-import Home from './components/Home/Home';
-import About from './components/About/About';
-import Portfolio from './components/Portfolio/Portfolio';
-import Skills from './components/Skills/Skills';
-import Contact from './components/Contact/Contact';
-import Header from "./components/Header/Header";
-import LSideSocialIcon from "./components/LSideSocialIcon/LSideSocialIcon";
-
 
 function App() {
   return (
     <div className="App">
-        <Header />
-        <LSideSocialIcon />
-
-        <Element className="section" name="home">
-          <Home />
-        </Element>
-        <Element className="section" name="about">
-          <About />
-        </Element>
-        <Element className="section" name="portfolio">
-          <Portfolio />
-        </Element>
-        <Element className="section" name="skills">
-          <Skills />
-        </Element>
-        <Element className="section" name="contact">
-          <Contact />
-        </Element>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
-};
+}
 
 export default App;
